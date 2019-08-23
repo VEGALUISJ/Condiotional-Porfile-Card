6a7de72472565cc7fb7be4b5f5a747e9ceefe0c5
VEGALUISJ <Luisvega.jlvv@gmail.com>
2019-08-23 23:03:35 +0000

    initial commit

--- public/bundle.js
+++ public/bundle.js
@@ -701,30 +701,59 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  let pName = "Your Name";
+  if (variables.name !== null) pName = variables.name;
+
+  let pLast = "Your Last Name";
+  if (variables.lastname !== null) pLast = variables.lastname;
+
+  let pRol = "Your Role";
+  if (variables.role !== null) pRol = variables.role;
+
+  let pCiudad = "Your City";
+  if (variables.city !== null) pCiudad = variables.city;
+
+  let pPais = "Your Country";
+  if (variables.country !== null) pPais = variables.country;
+
+  let pPos = "";
+  if (variables.socialMediaPosition !== null) pPos = variables.socialMediaPosition;
+
+  let pTwit = "";
+  if (variables.twitter !== null) pTwit = variables.twitter;
+
+  let pGit = "";
+  if (variables.github !== null) pGit = variables.github;
+
+  let pLink = "";
+  if (variables.linkedin !== null) pLink = variables.linkedin;
+
+  let pIns = "";
+  if (variables.instagram !== null) pIns = variables.instagram;
+
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+        ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${pName} ${pLast} </h1>
+          <h2>${pRol}</h2>
+          <h3>${pCiudad}, ${pPais}</h3>
+          <ul class="${pPos}">
+            <li><a href="https://twitter.com/${pTwit}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${pGit}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${pLink}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${pIns}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
